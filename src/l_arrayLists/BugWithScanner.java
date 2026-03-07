950566124b51a9cddb5ad3f90ed45d614e479560
애페스 <uknaklz4@gmail.com>
2026-03-07 17:13:13 +0500

    ArrayList

--- src/l_arrayLists/BugWithScanner.java
+++ src/l_arrayLists/BugWithScanner.java
@@ -7,13 +7,20 @@ public class BugWithScanner {
         Scanner scanner = new Scanner(System.in);
         System.out.println("String: ");
         String a = scanner.nextLine();
-        System.out.println("int: ");
 
+
+
+
+        System.out.println("int: ");
         int x = scanner.nextInt();
 
         scanner.nextLine(); //Need to reboot buffer, when after input int goes String input.
+
         System.out.println("String: ");
         String b = scanner.nextLine();
+
+
+
         System.out.println(a);
         System.out.println(x);
         System.out.println(b);
